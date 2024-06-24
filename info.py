d248af4f33d24b63eb3be6e3dff59e2021144d14
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2024-06-24 12:52:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ def is_enabled(value, default):
 API_ID = int(environ.get('API_ID', '21723146'))
 API_HASH = environ.get('API_HASH', '07cd9c82699c28111cb33693ecbd9116')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6088261601:AAFUCUHztjmN_wBprFFSiASK-1Tsf0XpBg4')
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1030335104').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6133440326').split()]
 USERNAME = environ.get('USERNAME', 'https://t.me/Attitude2688')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001626107740'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/DD_Movies_Request')
@@ -40,11 +40,11 @@ URL = environ.get('URL', 'aks-file-to-link-525cd78edc50.herokuapp.com')
 # verify system vars
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1001626107740'))
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/bots_up")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/bots_up/76")
 TUTORIAL2 = environ.get("TUTORIAL2", "https://t.me/bots_up/165")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "542b60b44c1d5d51c66a8c39bf51b5570afc0042")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "moneykamalo.com")
+SHORTENER_API = environ.get("SHORTENER_API", "88eadd498a2011ec3018ddef284d85662d0f2f68")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "zipshort.net")
 SHORTENER_API2 = environ.get("SHORTENER_API2", "388ed97f90606d81da656088cc6721395b699bba")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "ziplinker.net")
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "600"))
@@ -52,7 +52,7 @@ TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "600"))
 # languages search
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
-auth_channel = environ.get('AUTH_CHANNEL', '-1001626107740')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001954276464')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1001626107740'))
 
@@ -61,7 +61,7 @@ request_channel = environ.get('REQUEST_CHANNEL', '-1001626107740')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 
 # bot settings
-IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
+IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', True)
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '8080')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
